df109981a6b69d80353f6751464a2460050b2554
Rafiqul Islam <54430985+rafiqul570@users.noreply.github.com>
2024-02-27 22:30:31 +0600

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -145,10 +145,6 @@
         </nav>
         <div class="navicon-right">
           <a id="btnRightMenu" href="" class="pos-relative">
-            <i class="icon ion-ios-bell-outline"></i>
-            <!-- start: if statement -->
-            <span class="square-8 bg-danger"></span>
-            <!-- end: if statement -->
           </a>
         </div><!-- navicon-right -->
       </div><!-- sl-header-right -->
