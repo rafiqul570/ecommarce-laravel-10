0d6d26decfce9426a28d79e0fdeba1541fd59391
Rafiqul Islam <54430985+rafiqul570@users.noreply.github.com>
2024-02-27 19:39:27 +0600

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -120,20 +120,37 @@
         <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href=""><i class="icon ion-navicon-round"></i></a></div>
         <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href=""><i class="icon ion-navicon-round"></i></a></div>
       </div><!-- sl-header-left -->
-
-      <div class="sl-header-right" style="margin-right: 40px;">
+      <div class="sl-header-right">
         <nav class="nav">
-         <!-- Authentication -->
-         <form method="POST" action="{{ route('logout') }}">
-            @csrf
+          <div class="dropdown">
+          <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
+              <span class="logged-name text-light mr-3">{{ Auth::user()->name }}</span></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-header wd-200">
+              <ul class="list-unstyled user-profile-nav">
+                <li><a href="
+                <form method="POST" action="{{ route('logout') }}">
+                @csrf
 
-            <x-dropdown-link :href="route('logout')"
-                    onclick="event.preventDefault();
-                                this.closest('form').submit();">
-                {{ __('Log out') }}
-            </x-dropdown-link>
-        </form>
+                <x-dropdown-link :href="route('logout')"
+                        onclick="event.preventDefault();
+                                    this.closest('form').submit();">
+                    {{ __('Log out') }}
+                </x-dropdown-link>
+              </form>
+                "><i class="icon ion-power"></i> Sign Out</a></li>
+              </ul>
+            </div><!-- dropdown-menu -->
+          </div><!-- dropdown -->
         </nav>
+        <div class="navicon-right">
+          <a id="btnRightMenu" href="" class="pos-relative">
+            <i class="icon ion-ios-bell-outline"></i>
+            <!-- start: if statement -->
+            <span class="square-8 bg-danger"></span>
+            <!-- end: if statement -->
+          </a>
+        </div><!-- navicon-right -->
       </div><!-- sl-header-right -->
     </div><!-- sl-header -->
   <!-- ########## END: HEAD PANEL ########## -->
